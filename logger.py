74b9e9ce6ab42c03562fea1b61f80dbe5ca9ebfa
agus24 <agusx244@gmail.com>
2021-01-28 13:32:20 +0700

    update logger

--- logger.py
+++ logger.py
@@ -1,9 +1,13 @@
+import os
 from datetime import datetime
 
 
 class Logger:
     def __init__(self, user_id, file_name):
         self.user_id = user_id
+        file_path = os.getcwd() + "\\logs"
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
         self.file_name = f"logs/{file_name}"
 
     def log(self, text="-"):
